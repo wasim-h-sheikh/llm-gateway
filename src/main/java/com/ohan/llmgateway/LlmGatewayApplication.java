56ecadce1528e35ee992b77338ab6fd1b46cafc4
Wasim Sheikh <wasim.husen.sheikh@gmail.com>
2026-03-05 13:46:25 +0530

    Initial commit

---
+++ src/main/java/com/ohan/llmgateway/LlmGatewayApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2026 Wasim Sheikh
+ * Project: LLM Gateway
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+package com.ohan.llmgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohan/llmgateway/LlmGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohan.llmgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
