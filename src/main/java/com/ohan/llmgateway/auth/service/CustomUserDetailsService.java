74c958b79d01eeff01748d855129e2556c3bbfe5
Wasim Sheikh <wasim.husen.sheikh@gmail.com>
2026-03-05 14:07:56 +0530

    user db created.

---
+++ src/main/java/com/ohan/llmgateway/auth/service/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+/*
+ *
+ *  * Copyright (c) 2026 Wasim Sheikh
+ *  * Project: LLM Gateway
+ *  *
+ *  * Unauthorized copying of this file, via any medium is strictly prohibited.
+ *  * Proprietary and confidential.
+ *
+ */
+
+package com.ohan.llmgateway.auth.service;
+
+import com.ohan.llmgateway.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        var user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .disabled(!user.getEnabled())
+                .build();
+    }
+}
---
+++ src/main/java/com/ohan/llmgateway/config/PasswordConfig.java
@@ -0,0 +1,26 @@
+/*
+ *
+ *  * Copyright (c) 2026 Wasim Sheikh
+ *  * Project: LLM Gateway
+ *  *
+ *  * Unauthorized copying of this file, via any medium is strictly prohibited.
+ *  * Proprietary and confidential.
+ *
+ */
+
+package com.ohan.llmgateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
